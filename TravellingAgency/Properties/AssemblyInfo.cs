ef3fb2f8d4be1cb0db2d337d9a0460c1c10a652b
Aleksandra Stoycheva <astojceva@yahoo.com>
2016-11-02 17:43:23 +0200

    Upgrade to 9.2.6200.0

--- TravellingAgency/Properties/AssemblyInfo.cs
+++ TravellingAgency/Properties/AssemblyInfo.cs
@@ -37,6 +37,8 @@ using System.Resources;
 [assembly: AssemblyFileVersion("7.1.5150.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
 
+
+
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ControllerContainer]
 
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ResourcePackage]
